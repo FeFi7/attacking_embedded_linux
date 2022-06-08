19b568d460ecc18d1813332d2f5aea27c85d9a00
Felix Filser <72125556+FeFi7@users.noreply.github.com>
2022-06-08 14:21:57 +0200

    Update start.sh

--- poky/start.sh
+++ poky/start.sh
@@ -1,1 +1,10 @@
-/home/felix/poky/build/tmp/work/x86_64-linux/qemu-helper-native/1.0-r1/recipe-sysroot-native/usr/bin/qemu-system-x86_64 -device virtio-net-pci,netdev=net0,mac=52:54:00:12:34:02 -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -drive file=/home/felix/poky/build/tmp/deploy/images/qemux86-64/core-image-minimal-qemux86-64-20220603081647.rootfs.ext4,if=virtio,format=raw -usb -device usb-tablet   -cpu IvyBridge -machine q35 -smp 4 -m 256 -serial mon:vc -serial null  -kernel /home/felix/poky/build/tmp/deploy/images/qemux86-64/bzImage--5.15.22+git0+2d38a472b2_7f685244af-r0-qemux86-64-20220519165043.bin -append 'root=/dev/vda rw  mem=256M ip=192.168.7.2::192.168.7.1:255.255.255.0::eth0:off:8.8.8.8 oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1 '
+#!/bin/bash
+qemu-system-x86_64 \
+-device virtio-net-pci,netdev=net0,mac=52:54:00:12:34:02 -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+-object rng-random,filename=/dev/urandom,id=rng0 \
+-device virtio-rng-pci,rng=rng0 \
+-drive file=images/qemux86-64/core-image-minimal-qemux86-64-20220603112642.rootfs.ext4,if=virtio,format=raw \
+-usb -device usb-tablet   \
+-cpu IvyBridge -machine q35 -smp 4 -m 256 -serial mon:vc -serial null  \
+-kernel images/qemux86-64/bzImage--5.15.22+git0+2d38a472b2_7f685244af-r0-qemux86-64-20220519165043.bin \
+-append 'root=/dev/vda rw  mem=256M ip=192.168.7.2::192.168.7.1:255.255.255.0::eth0:off:8.8.8.8 oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1 ' 
