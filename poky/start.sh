fb9fd87ca82ac12b0f7430e2bd326147137ce414
Felix Filser <72125556+FeFi7@users.noreply.github.com>
2022-06-14 11:54:23 +0200

    Update start.sh

--- poky/start.sh
+++ poky/start.sh
@@ -2,7 +2,7 @@
 qemu-system-x86_64 \
 -object rng-random,filename=/dev/urandom,id=rng0 \
 -device virtio-rng-pci,rng=rng0 \
--drive file=images/qemux86-64/core-image-minimal-qemux86-64-20220603112642.rootfs.ext4,if=virtio,format=raw \
+-drive file=images/qemux86-64/core-image-minimal-qemux86-64-20220612211642.rootfs.ext4,if=virtio,format=raw \
 -usb -device usb-tablet   \
 -cpu IvyBridge -machine q35 -smp 4 -m 256 -serial mon:vc -serial null  \
 -kernel images/qemux86-64/bzImage--5.15.22+git0+2d38a472b2_7f685244af-r0-qemux86-64-20220519165043.bin \
